5a8d150767cf5757e15933b3dab7f75bad9aadd7
Devashish Priyadarshi <devashish234073@gmail.com>
2023-12-31 21:03:52 +0530

    formatted the text in controller

--- src/com/devashish/controller/MyController.java
+++ src/com/devashish/controller/MyController.java
@@ -9,13 +9,15 @@ import com.devashish.framework.injectable.HttpHeader;
 
 @RestController
 public class MyController {
-    @GetMapping("/hello1")
-    public String sayHello1() {
-        return "<h1>name is ${name} and id is ${id}</h1>, <input type='text'>";
-    }
-    
-    @GetMapping("/hello2")
-    public String sayHello2(@RequestParam("name") String name,HttpHeader httpHeaders) {
-    	return "Hello2, <input type='text' value='"+name.toUpperCase()+"'><p>"+httpHeaders.get("user-agent")+"</p><p>"+httpHeaders.get("Host")+"</p>";
-    }
+	@GetMapping("/hello1")
+	public String sayHello1() {
+		return "<h1>name is ${name} and id is ${id}</h1>, <input type='text'>";
+	}
+
+	@GetMapping("/hello2")
+	public String sayHello2(@RequestParam("name") String name, HttpHeader httpHeaders) {
+		return "Hello2, <input type='text' value='" + name.toUpperCase() 
+		        + "'><p>" + httpHeaders.get("user-agent")
+				+ "</p><p>" + httpHeaders.get("Host") + "</p>";
+	}
 }
